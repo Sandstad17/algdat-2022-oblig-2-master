2a37f7f202429a3c3f478068aaa350415afda4ce
even44 <even.andreas.ingeberg@ebnett.no>
2022-10-04 13:25:57 +0200

    Update Main_For_Test.java

--- src/main/java/no/oslomet/cs/algdat/Oblig2/Main_For_Test.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/Main_For_Test.java
@@ -3,9 +3,15 @@ package no.oslomet.cs.algdat.Oblig2;
 public class Main_For_Test {
     public static void main(String[] args) {
 
-        String[] s = {"Ole", "kari", "NESTE", "Per", "Kari"};
-        Liste<String> liste = new DobbeltLenketListe<>(s);
-        System.out.println(liste.antall() + " " + liste.tom());
+        String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
+        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
+        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
+        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
 
+        System.out.println(l1.toString() + " " + l2.toString()
+                + " " + l3.toString() + " " + l1.omvendtString() + " "
+                + l2.omvendtString() + " " + l3.omvendtString());
+
+        // Utskrift: [] [A] [A, B] [] [A] [B, A]
     }
 }
