bc5f21569c4f35254672a74f3218185522c10da2
even44 <even.andreas.ingeberg@ebnett.no>
2022-10-10 13:14:34 +0200

    Revert "Update Main_For_Test.java"
    This reverts commit f4d1a020dca7a55721b40eb8d3772299ff62c156.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/Main_For_Test.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/Main_For_Test.java
@@ -3,16 +3,15 @@ package no.oslomet.cs.algdat.Oblig2;
 public class Main_For_Test {
     public static void main(String[] args) {
 
-        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
-        System.out.println(liste.toString() + " " + liste.omvendtString());
-        for (int i = 1; i <= 3; i++) {
-            liste.leggInn(i);
-            System.out.println(liste.toString() + " " + liste.omvendtString());
-        }
-        // Utskrift:
-        // [] []
-        // [1] [1]
-        // [1, 2] [2, 1]
-        // [1, 2, 3] [3, 2, 1]
+        String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null, "C"};
+        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
+        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
+        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
+
+        System.out.println(l1.toString() + " " + l2.toString()
+                + " " + l3.toString() + " " + l1.omvendtString() + " "
+                + l2.omvendtString() + " " + l3.omvendtString());
+
+        // Utskrift: [] [A] [A, B] [] [A] [B, A]
     }
 }
